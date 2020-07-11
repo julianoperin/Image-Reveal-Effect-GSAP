d407c10fa89932a149f443853c5e1f74beaf171e
‘Juliano <julianoperins@gmail.com>
2020-07-11 03:12:14 -0400

    Image with reveal effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import imagex from "./images/house.jpg";
+import "./App.scss";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import { TimelineLite, Power2 } from "gsap";
 
 function App() {
+  let container = useRef(null);
+  let image = useRef(null);
+  let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+
+  const tl = new TimelineLite();
+
+  useEffect(() => {
+    tl.to(container, 1, { css: { visibility: "visible" } })
+      .to(imageReveal, 1.4, { width: "0%", ease: Power2.easeOut })
+      .from(image, 1.4, { scale: 1.6, ease: Power2.easeInOut, delay: -1.6 });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="main">
+      <div className="container" ref={(el) => (container = el)}>
+        <>
+          <div className="img-container">
+            <img src={imagex} alt="img" ref={(el) => (image = el)} />
+          </div>
+        </>
+      </div>
+    </section>
   );
 }
 
